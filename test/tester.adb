da328b2c7a3bb8d5994f7d7d09cded7e51af4728
Dennis Przytarski <dennis@przytarski.com>
2014-02-15 17:22:10 +0100

    added string tests

--- test/tester.adb
+++ test/tester.adb
@@ -8,6 +8,7 @@ with Date.Test.Byte_Checker_i64;
 with Date.Test.Byte_Checker_v64;
 
 with Date.Test.Byte_Checker_Boolean;
+with Date.Test.Byte_Checker_String;
 
 with Date.Test.Parse_Test;
 
@@ -21,6 +22,7 @@ begin
    Ahven.Framework.Add_Test (Suite, new Date.Test.Byte_Checker_v64.Test);
 
    Ahven.Framework.Add_Test (Suite, new Date.Test.Byte_Checker_Boolean.Test);
+   Ahven.Framework.Add_Test (Suite, new Date.Test.Byte_Checker_String.Test);
 
    Ahven.Framework.Add_Test (Suite, new Date.Test.Parse_Test.Test);
    Ahven.Text_Runner.Run (Suite);
