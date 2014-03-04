01bb0a8c3e5c42b5f51e2cec80abbad76db9857e
Dennis Przytarski <dennis@przytarski.com>
2014-03-04 20:47:20 +0100

    removed unnecessary files

--- date copy/date-api.ads
+++
@@ -1,9 +0,0 @@
---  ___ _  ___ _ _                                                            --
--- / __| |/ (_) | |       Your SKilL Ada Binding                              --
--- \__ \ ' <| | | |__     <<debug>>                                           --
--- |___/_|\_\_|_|____|    by: <<some developer>>                              --
---                                                                            --
-
-package Date.Api is
-
-end Date.Api;
